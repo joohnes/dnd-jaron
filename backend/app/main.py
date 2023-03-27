903191bf8eeebdf2509fd7fe7ea513c442da982f
joohnes <kubapienkowski@gmail.com>
2023-03-27 15:02:11 +0200

    created simple structure

---
+++ backend/app/main.py
@@ -0,0 +1,22 @@
+from api.routers import api_router
+from db.base import Base
+from db.session import engine
+from fastapi import APIRouter
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+root_router = APIRouter()
+app.include_router(api_router, prefix="/api")
+app.include_router(root_router)
